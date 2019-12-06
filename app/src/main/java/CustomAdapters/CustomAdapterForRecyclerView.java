b7747939389f5496463c4416f59dfe052c6f06f5
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:17:40 -0500

    [Harmanpreet Singh] customeadaptor with all

--- app/src/main/java/CustomAdapters/CustomAdapterForRecyclerView.java
+++ app/src/main/java/CustomAdapters/CustomAdapterForRecyclerView.java
@@ -20,5 +20,78 @@ public class CustomAdapterForRecyclerView extends RecyclerView.Adapter<CustomAda
 
 
 
-    
+    public interface SetCustomClickListener{
+        public  void customOnClick(Employee e);
+    }
+
+
+
+    ArrayList<Employee> arrayList;
+    Context context;
+    SetCustomClickListener listener;
+
+
+    public CustomAdapterForRecyclerView(ArrayList<Employee> arrayList, Context context, SetCustomClickListener listener) {
+        this.arrayList = arrayList;
+        this.context = context;
+        this.listener = listener;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(this.context);
+        View view = inflater.inflate(R.layout.card_layout_employee, viewGroup , false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+            viewHolder.bind(this.arrayList.get(i), this.listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.arrayList.size();
+    }
+
+    class ViewHolder extends  RecyclerView.ViewHolder{
+
+        TextView name;
+        TextView gender;
+        TextView age;
+
+
+        public ViewHolder(@NonNull View View) {
+            super(View);
+            name = itemView.findViewById(R.id.emp_name);
+            gender = itemView.findViewById(R.id.emp_gender);
+            age = itemView.findViewById(R.id.emp_age);
+        }
+
+
+        public  void bind(final Employee e , final SetCustomClickListener listener)
+        {
+            if(e.getGen() == Gender.MALE )
+            {
+                gender.setText("MALE");
+            }
+            else
+            {
+                gender.setText("FEMALE");
+            }
+
+            name.setText("Name : "+e.getName());
+            age.setText("Age : "+e.getAge());
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        listener.customOnClick(e);
+                    }
+                });
+        }
+    }
 }
