d439ef564113520e20d03a9e4bee3e6e2d453bb8
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 21:11:27 -0500

    [Varinder] datepicker with methods

--- app/src/main/java/com/camy/androidfinal/DatePickerFragment.java
+++ app/src/main/java/com/camy/androidfinal/DatePickerFragment.java
@@ -14,5 +14,28 @@ import java.util.Calendar;
 public class DatePickerFragment extends DialogFragment  {
 
 
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        final int day = cal.get(Calendar.DAY_OF_MONTH);
 
+        return new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int yyyy, int mm, int dd) {
+              //  Toast.makeText(getContext(),"year "+year+" month "+month+" day "+day, Toast.LENGTH_LONG).show();
+
+              TextView text_dob =  getActivity().findViewById(R.id.text_date_of_birth);
+              //text_age.setText("DateOfBirth : "+year+"/"+month+"/"+day);
+                int age = Calendar.getInstance().get(Calendar.YEAR) - yyyy;
+
+                TextView text_age = getActivity().findViewById(R.id.text_age);
+                text_age.setText("Age : "+age);
+
+              text_dob.setText("DateOfBirth : "+yyyy+"/"+(mm+1)+"/"+dd);
+            }
+        },year,month,day);
+    }
 }
