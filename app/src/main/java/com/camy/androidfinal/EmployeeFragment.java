6f65dc8876c0d6fa7cb660fc054c5a2dc174f7f5
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:25:56 -0500

    [Harmanpreet Singh] employeefragment components decalration and logicd and func

--- app/src/main/java/com/camy/androidfinal/EmployeeFragment.java
+++ app/src/main/java/com/camy/androidfinal/EmployeeFragment.java
@@ -32,4 +32,64 @@ public class EmployeeFragment extends Fragment   {
     FragmentTransaction fragmentTransaction;
     EmployeeDatailsFragment employeeDatailsFragment;
 
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.con = inflater.getContext();
+
+        View v =  inflater.inflate(R.layout.employee_fragment_layout, container, false);
+
+
+        employees_list = SingleToneExample.getObj().getList();
+
+        this.recyclerView = v.findViewById(R.id.recycler_view);
+        adapter = new CustomAdapterForRecyclerView(employees_list, this.con, new CustomAdapterForRecyclerView.SetCustomClickListener() {
+            @Override
+            public void customOnClick(Employee employee) {
+
+                Toast.makeText(EmployeeFragment.this.con, employee.getName(), Toast.LENGTH_LONG).show();
+                if(employeeDatailsFragment == null) {
+                     employeeDatailsFragment = new EmployeeDatailsFragment();
+                }
+                 employeeDatailsFragment.employeeObject(employee);
+                 fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                 fragmentTransaction.replace(R.id.frame_layout , employeeDatailsFragment , "");
+                 fragmentTransaction.commit();
+                 fragmentTransaction.addToBackStack(null);
+
+            }
+        });
+        this.recyclerView.setAdapter(adapter);
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(this.con));
+
+        return v;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+
+        super.onAttach(context);
+    }
+
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+
+
+
+    }
+
+
+
 }
