2fc1a87fe2a78199ab2ffb95ba96a84ce3a9afce
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:45:27 -0500

    [Harmanpreet Singh] intern fragment and layout

---
+++ app/src/main/java/com/camy/androidfinal/InternFragment.java
@@ -0,0 +1,27 @@
+package com.camy.androidfinal;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.camy.androidfinal.interfaces.DataFromAddEmployeeFragment;
+
+import model.Car;
+import model.Intern;
+import model.MotorCycle;
+import model.SingleToneExample;
+import model.abstracts.Vehicle;
+import model.enums.Gender;
+
+public class InternFragment extends Fragment implements DataFromAddEmployeeFragment {
+
+    
+}
