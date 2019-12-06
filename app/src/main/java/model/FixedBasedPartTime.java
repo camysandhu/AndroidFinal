50e08fa55ebfe5ae8f064d2225d28bf50a8be9c4
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:10:38 -0500

    [Harmanpreet Singh] class fixedBased with all methods

--- app/src/main/java/model/FixedBasedPartTime.java
+++ app/src/main/java/model/FixedBasedPartTime.java
@@ -3,9 +3,29 @@ package model;
 
 
 import model.abstracts.PartTime;
+import model.abstracts.Vehicle;
+import model.enums.Gender;
 
 public class FixedBasedPartTime  extends PartTime {
 
-		
+    private int fixedamount;
+
+    public FixedBasedPartTime(int fixedamount, int rate, float hours , String name, int age, Gender gen , Vehicle vehicle) {
+        super( rate,  hours ,  name,  age, gen , vehicle);
+        this.fixedamount = fixedamount;
+    }
+
+    public int getFixedamount() {
+        return fixedamount;
+    }
+
+    public void setFixedamount(int fixedamount) {
+        this.fixedamount = fixedamount;
+    }
+
+    public float fixedAmountCalcEarnings()
+    {
+        return this.getFixedamount()+((float)this.getHours()*this.getRate());
+    }
 
 }
