26d38e2d27e0ee9fafe308b48610394cf45b9df4
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:12:38 -0500

    [Harmanpreet Singh] class intern with attributes

--- app/src/main/java/model/Intern.java
+++ app/src/main/java/model/Intern.java
@@ -7,12 +7,32 @@ import model.abstracts.Vehicle;
 import model.enums.Gender;
 
 public class Intern extends Employee {
-		
-		
+    private String schoolname;
+
+    public Intern() {
+        super();
+    }
 
 
 
-		
-		
-		
+    public Intern(String schoolname, String name, int age , Gender gen , Vehicle vehicle ) {
+        super(name,age, gen , vehicle);
+        this.schoolname = schoolname;
+    }
+
+    public String getSchoolname() {
+        return schoolname;
+    }
+
+    public void setSchoolname(String schoolname) {
+        this.schoolname = schoolname;
+    }
+
+
+
+
+
+
+
+
 }
