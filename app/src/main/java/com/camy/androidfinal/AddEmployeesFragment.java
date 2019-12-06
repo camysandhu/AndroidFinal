5da8e3449d6505e37f24b9bcf1f6577049ef477c
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 21:38:13 -0500

    [Varinder] addemployeefragment ui components find view by id

--- app/src/main/java/com/camy/androidfinal/AddEmployeesFragment.java
+++ app/src/main/java/com/camy/androidfinal/AddEmployeesFragment.java
@@ -25,7 +25,7 @@ public class AddEmployeesFragment extends Fragment implements View.OnClickListen
     FullTimeFragment fullTimeFragment;
     PartTimeFragment partTimeFragment;
     InternFragment internFragment;
-
+    FragmentManager fragmentManager;
 
     @Nullable
     @Override
@@ -37,7 +37,19 @@ public class AddEmployeesFragment extends Fragment implements View.OnClickListen
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        this.vehicle = view.findViewById(R.id.radio_group_vehicle);
+        this.text_age = view.findViewById(R.id.text_age);
+        this.text_name = view.findViewById(R.id.text_name);
+        this.fragmentManager = this.getActivity().getSupportFragmentManager();
 
+
+
+
+        this.text_date_of_birth = view.findViewById(R.id.text_date_of_birth);
+        this.text_date_of_birth.setText(this.text_date_of_birth.getText());
+        this.text_date_of_birth.setOnClickListener(this) ;
+
+        this.gender = view.findViewById(R.id.radio_group_gender);
     }
 
 
