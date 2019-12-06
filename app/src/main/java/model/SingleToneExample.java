3c0f3e114e465aa14ed011f3d3bb6565ff873fee
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:14:17 -0500

    [Harmanpreet Singh] class singleton

---
+++ app/src/main/java/model/SingleToneExample.java
@@ -0,0 +1,16 @@
+package model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.abstracts.Employee;
+import model.enums.Gender;
+
+
+public class SingleToneExample {
+			
+	
+
+			
+}
