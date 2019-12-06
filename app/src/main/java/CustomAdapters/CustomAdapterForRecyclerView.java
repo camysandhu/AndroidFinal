fea30176bd039b56f6ceb895c8f0f9f3a927625b
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:17:10 -0500

    [Harmanpreet Singh] customeadaptor

---
+++ app/src/main/java/CustomAdapters/CustomAdapterForRecyclerView.java
@@ -0,0 +1,24 @@
+package CustomAdapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.camy.androidfinal.R;
+
+import java.util.ArrayList;
+
+import model.abstracts.Employee;
+import model.enums.Gender;
+
+public class CustomAdapterForRecyclerView extends RecyclerView.Adapter<CustomAdapterForRecyclerView.ViewHolder> {
+
+
+
+
+    
+}
