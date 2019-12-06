6802f66f0ee2f63716f7c1f356eecd0323b5a7c9
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:33:27 -0500

    [Harmanpreet Singh] fixbasedFragment

---
+++ app/src/main/java/com/camy/androidfinal/FixBasedFragment.java
@@ -0,0 +1,26 @@
+package com.camy.androidfinal;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.camy.androidfinal.interfaces.DataFromPartTimeFragment;
+
+import model.Car;
+import model.FixedBasedPartTime;
+import model.MotorCycle;
+import model.SingleToneExample;
+import model.abstracts.Vehicle;
+import model.enums.Gender;
+
+public class FixBasedFragment extends Fragment implements DataFromPartTimeFragment {
+
+}
