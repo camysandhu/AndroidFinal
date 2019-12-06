5c1970e1b83dce49f92198cc6b862773047c4790
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:31:38 -0500

    [Harmanpreet Singh] interface datafromempdetails

---
+++ app/src/main/java/com/camy/androidfinal/interfaces/DateFromEmployeeDetailsFragment.java
@@ -0,0 +1,7 @@
+package com.camy.androidfinal.interfaces;
+
+import model.abstracts.Employee;
+
+public interface DateFromEmployeeDetailsFragment  {
+    public void employeeObject(Employee employee);
+}
