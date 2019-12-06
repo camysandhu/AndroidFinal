1fb5497a6376610e9a6f2afff57785a1135380ad
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:38:15 -0500

    [Harmanpreet Singh] fulltime fragment

---
+++ app/src/main/java/com/camy/androidfinal/FullTimeFragment.java
@@ -0,0 +1,28 @@
+package com.camy.androidfinal;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.camy.androidfinal.interfaces.DataFromAddEmployeeFragment;
+
+import model.Car;
+import model.FullTime;
+import model.MotorCycle;
+import model.SingleToneExample;
+import model.abstracts.Vehicle;
+import model.enums.Gender;
+
+public class FullTimeFragment extends Fragment implements DataFromAddEmployeeFragment {
+
+
+
+}
