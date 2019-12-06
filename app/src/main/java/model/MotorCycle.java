0368dc2d609a5c72caab19f92fa888d9fe97ceb2
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:13:37 -0500

    [Harmanpreet Singh] class motorcycle with methods

--- app/src/main/java/model/MotorCycle.java
+++ app/src/main/java/model/MotorCycle.java
@@ -5,10 +5,25 @@ package model;
 import model.abstracts.Vehicle;
 
 public class MotorCycle extends Vehicle {
-		
+    private String motomodelnumber;
 
-		
-		
-		
-		
+    public MotorCycle(String motomodelnumber,String make, String plate, int mileage) {
+        super(make, plate, mileage);
+        this.motomodelnumber = motomodelnumber;
+    }
+
+    public String getMotomodelnumber() {
+        return motomodelnumber;
+    }
+
+    public void setMotomodelnumber(String motomodelnumber) {
+        this.motomodelnumber = motomodelnumber;
+    }
+
+
+
+
+
+
+
 }
