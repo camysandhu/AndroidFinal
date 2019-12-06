82bc497f8c51b1aa53a06ce34a111873c0e09697
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:30:41 -0500

    [Harmanpreet Singh] interface datafromemp

---
+++ app/src/main/java/com/camy/androidfinal/interfaces/DataFromAddEmployeeFragment.java
@@ -0,0 +1,14 @@
+package com.camy.androidfinal.interfaces;
+
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public interface DataFromAddEmployeeFragment
+{
+
+    public void viewsFromAddEmployeeFragment(TextView name, TextView age, RadioGroup gender , TextView date , RadioGroup vehicle);
+
+
+
+
+}
