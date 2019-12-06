c221f9492e7c7f0495da0c202e5d88f8f08d6aa0
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:11:36 -0500

    [Harmanpreet Singh] class fulltime with methods

--- app/src/main/java/model/FullTime.java
+++ app/src/main/java/model/FullTime.java
@@ -7,8 +7,45 @@ import model.abstracts.Vehicle;
 import model.enums.Gender;
 
 public class FullTime extends Employee {
-	
+    private int salary;
+    private int bonus;
 
-	
-	
+
+
+
+    public FullTime(int salary, int bonus, String name, int age, Gender gen, Vehicle vehicle) {
+        super(name,age, gen ,vehicle);
+        this.salary = salary;
+        this.bonus = bonus;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
+
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public int calcEarning() {
+
+        return this.getSalary()+this.getBonus();
+    }
+
+
+
+
+
 }
