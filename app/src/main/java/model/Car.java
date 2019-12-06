ec432ef54e83fc948685548e177191e8be75f979
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:07:20 -0500

    [Harmanpreet Singh] class car all methods

--- app/src/main/java/model/Car.java
+++ app/src/main/java/model/Car.java
@@ -2,10 +2,27 @@ package model;
 
 
 import model.abstracts.Vehicle;
+import model.enums.Gender;
 
 public class Car extends Vehicle {
 
 
+    private String carmodelnumber;
 
-			
+
+
+    public Car(String carmodelnumber,String make, String plate, int mileage) {
+        super(make, plate, mileage);
+        this.carmodelnumber = carmodelnumber;
+    }
+
+    public String getCarmodelnumber() {
+        return carmodelnumber;
+    }
+
+    public void setCarmodelnumber(String carmodelnumber) {
+        this.carmodelnumber = carmodelnumber;
+    }
+
+
 }
