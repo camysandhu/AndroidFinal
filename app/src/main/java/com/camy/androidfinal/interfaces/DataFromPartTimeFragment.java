00e544b121ffeaf189fb06c05d633ea2e01713ec
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:31:15 -0500

    [Harmanpreet Singh] interface datafromparttime

---
+++ app/src/main/java/com/camy/androidfinal/interfaces/DataFromPartTimeFragment.java
@@ -0,0 +1,9 @@
+package com.camy.androidfinal.interfaces;
+
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public interface DataFromPartTimeFragment  {
+
+    public void viewsFromPartTimeFragment(TextView name, TextView age , RadioGroup gender, TextView ratePerHour, TextView numberOfHours , TextView dateOfBirth , RadioGroup vehicle);
+}
