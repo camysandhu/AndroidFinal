70702bbba145d50dc807b5e53d477770cb724dec
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 21:12:37 -0500

    [Varinder] employeeDetails fragment

---
+++ app/src/main/java/com/camy/androidfinal/EmployeeDatailsFragment.java
@@ -0,0 +1,32 @@
+package com.camy.androidfinal;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.camy.androidfinal.interfaces.DateFromEmployeeDetailsFragment;
+
+import model.Car;
+import model.CommissionBasedPartTime;
+import model.FixedBasedPartTime;
+import model.FullTime;
+import model.Intern;
+import model.abstracts.Employee;
+import model.abstracts.PartTime;
+import model.enums.Gender;
+
+public class EmployeeDatailsFragment extends Fragment implements DateFromEmployeeDetailsFragment {
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
