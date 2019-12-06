9688c00e43d8dc77ab6e73a39b4f9c5ddfdea483
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:12:09 -0500

    [Harmanpreet Singh] class intern

---
+++ app/src/main/java/model/Intern.java
@@ -0,0 +1,18 @@
+package model;
+
+
+
+import model.abstracts.Employee;
+import model.abstracts.Vehicle;
+import model.enums.Gender;
+
+public class Intern extends Employee {
+		
+		
+
+
+
+		
+		
+		
+}
