a3f50f38ef7124d8019084f7d3862fabcd2b6639
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:03:04 -0500

    [Harmanpreet Singh] all methods in emoployee class

--- app/src/main/java/model/abstracts/Employee.java
+++ app/src/main/java/model/abstracts/Employee.java
@@ -1,6 +1,9 @@
 package model.abstracts;
 
 
+import java.util.Calendar;
+
+import model.UserDefinedExe;
 import model.enums.Gender;
 
 public abstract class Employee  {
@@ -9,5 +12,74 @@ public abstract class Employee  {
 	private Gender gen;
 
 	private Vehicle vehicle;
+    public Employee()
+    {
+
+    }
+
+    public Employee(String name, int age, Gender gen, Vehicle vehicle) {
+        super();
+        Name = name;
+        this.age = age;
+        this.vehicle= vehicle;
+        this.gen= gen;
+    }
+
+
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public int getAge() {
+
+
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int calcBirthYear()
+    {
+
+        try {
+
+
+            if(this.getAge() < 0)
+            {
+                throw new UserDefinedExe("Age is negative");
+
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return (Calendar.getInstance().get(Calendar.YEAR)-this.getAge());
+    }
+
+    public int calcEarning()
+    {
+        return 1000;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+    public Gender getGen() {
+        return gen;
+    }
+
+    public void setGen(Gender gen) {
+        this.gen = gen;
+    }
+
 
 }
+
