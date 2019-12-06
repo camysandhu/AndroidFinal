7287bd6f5bd19f355f5b6067f26ebb9f73dccb8f
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 21:37:13 -0500

    [Varinder] addemployeefragment

---
+++ app/src/main/java/com/camy/androidfinal/AddEmployeesFragment.java
@@ -0,0 +1,36 @@
+package com.camy.androidfinal;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AddEmployeesFragment extends Fragment implements View.OnClickListener {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.add_employee_fragment_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+
+
+}
