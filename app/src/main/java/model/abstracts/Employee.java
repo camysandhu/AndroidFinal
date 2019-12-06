110b7ec3d83c28f4de3cab47ffb71dcb964f5ea5
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 20:01:56 -0500

    [Harmanpreet Singh] variables in emoployee class

---
+++ app/src/main/java/model/abstracts/Employee.java
@@ -0,0 +1,13 @@
+package model.abstracts;
+
+
+import model.enums.Gender;
+
+public abstract class Employee  {
+	private String Name;
+	private int age;
+	private Gender gen;
+
+	private Vehicle vehicle;
+
+}
