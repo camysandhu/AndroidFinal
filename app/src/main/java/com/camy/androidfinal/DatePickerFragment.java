4af681168944310087bf4d994787f1fc1e34bb0e
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 21:10:44 -0500

    [Varinder] datepicker

---
+++ app/src/main/java/com/camy/androidfinal/DatePickerFragment.java
@@ -0,0 +1,18 @@
+package com.camy.androidfinal;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment  {
+
+
+
+}
