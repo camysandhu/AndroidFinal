f74a694f144fbfe790bb1fba78e035b8169d88bf
Komal2006 <komaldeepkr99@gmail.com>
2019-12-05 21:04:44 -0500

    [Varinder] parttime fragment

---
+++ app/src/main/java/com/camy/androidfinal/PartTimeFragment.java
@@ -0,0 +1,39 @@
+package com.camy.androidfinal;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.camy.androidfinal.interfaces.DataFromAddEmployeeFragment;
+
+public class PartTimeFragment extends Fragment implements DataFromAddEmployeeFragment {
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+
+
+
+    }
+}
